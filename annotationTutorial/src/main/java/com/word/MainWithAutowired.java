b2e9c080c65ec3805e33f6167f168760a651662f
youjin <youjin@218.38.137.28>
2019-11-12 14:46:31 +0900

    new GenericXmlApplicationContext("classpath:") 와 getBean()

---
+++ annotationTutorial/src/main/java/com/word/MainWithAutowired.java
@@ -0,0 +1,36 @@
+package com.word;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.word.service.WordRegister;
+import com.word.service.WordRegisterWithAutowired;
+import com.word.service.WordSearch;
+import com.word.service.WordSearchWithAutowired;
+
+public class MainWithAutowired {
+
+	public static void main(String[] args) {
+
+		GenericXmlApplicationContext ctx = new	GenericXmlApplicationContext("classpath:appContextWithAutowired.xml");
+		
+		// 단어 등록 
+		WordRegisterWithAutowired wordRegister = ctx.getBean("wordRegister", WordRegisterWithAutowired.class);
+		
+		String key = "개발자";
+		String value = "최유진";
+		WordSet wordSet = new WordSet(key, value);
+		wordRegister.register(wordSet);
+		
+		System.out.println("---------------");
+		
+		// 단어 검색
+		WordSearchWithAutowired wordSearch = ctx.getBean("wordSearch", WordSearchWithAutowired.class);
+		wordSet = wordSearch.search(key); // 검색 결과는 WordSet 객체 자료형 
+		System.out.println("키 : " + wordSet.getWordKey());
+		System.out.println("값 : " + wordSet.getWordValue());
+		
+		ctx.close();
+		
+	}
+
+}
