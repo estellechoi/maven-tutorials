387efa2dc304f6956f4ce96eb44b1974a9c77c59
youjin <youjin@218.38.137.28>
2019-11-12 10:13:06 +0900

    new GenericXmlApplicationContext("classpath:") 와 getBean() 메소드

--- first/src/main/java/first/Main.java
+++ first/src/main/java/first/Main.java
@@ -1,11 +1,23 @@
 package first;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Test test = new Test();
+//		Test test = new Test();
+//		test.go();
+		
+		// applicationContext.xml 에서 만든 빈 객체 사용하기 
+		// 매개변수로 빈 객체를 생성한 파일명을 넣어준다 
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationContext.xml");
+		
+		// getBean(빈id, 빈을 담을 자료형) 메소드로 객체 불러오기 
+		Test test = ctx.getBean("test", Test.class);
 		test.go();
+		
+		ctx.close();
 	}
 
 }
