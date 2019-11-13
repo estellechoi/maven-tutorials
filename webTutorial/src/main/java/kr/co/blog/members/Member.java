94251c5c00c4136de2017548cb8b7eeaa0ea9964
youjin <youjin@192.168.31.152>
2019-11-13 17:48:29 +0900

    DTO / <form> 파라미터와 동일이름 변수는 커맨드 객체를 이용해 전송정보 얻을 수 있다 !

--- webTutorial/src/main/java/kr/co/blog/members/Member.java
+++ webTutorial/src/main/java/kr/co/blog/members/Member.java
@@ -5,7 +5,11 @@ public class Member {
 	private String email;
 	private String pwd;
 	private String username;
-	private String cell;
+	private String cell1;
+	private String cell2;
+	private String cell3;
+	
+	
 	public String getEmail() {
 		return email;
 	}
@@ -24,14 +28,28 @@ public class Member {
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	public String getCell() {
-		return cell;
+	public String getCell1() {
+		return cell1;
 	}
-	public void setCell(String cell) {
-		this.cell = cell;
+	public void setCell1(String cell1) {
+		this.cell1 = cell1;
+	}
+	public String getCell2() {
+		return cell2;
+	}
+	public void setCell2(String cell2) {
+		this.cell2 = cell2;
+	}
+	public String getCell3() {
+		return cell3;
+	}
+	public void setCell3(String cell3) {
+		this.cell3 = cell3;
 	}
 	
 	
+	
+
 	
 
 }
