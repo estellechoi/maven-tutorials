b63242986c2e1f4907aefae293de4ef4f57c3783
youjin <youjin@192.168.31.152>
2019-11-13 17:49:01 +0900

    커맨드 객체를 이용한 HTTP 전송정보 얻기 (HttpServletRequest, Model을 대체)

--- webTutorial/src/main/java/kr/co/blog/MemberController.java
+++ webTutorial/src/main/java/kr/co/blog/MemberController.java
@@ -1,4 +1,4 @@
-package kr.co.blog.members.controller;
+package kr.co.blog;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -8,10 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.co.blog.members.Member;
 import kr.co.blog.members.service.MemberService;
 
 @Controller
+@RequestMapping("/member") // 공통경로 명시 (중복 데이터 제거)
 public class MemberController {
 
 	// 컨테이너에서 동일타입(MemberService) 빈 객체 호출
@@ -20,33 +23,46 @@ public class MemberController {
 	MemberService memberService;
 
 	@RequestMapping(value = "/signin", method = RequestMethod.POST)
-	public String signin(Model model, HttpServletRequest request) {
-		String email = request.getParameter("email");
-		String pwd = request.getParameter("pwd");
+	public String signin(Model model,
+			@RequestParam("email") String email,
+			@RequestParam(value = "pwd", required = false, defaultValue = "0000") String pwd) {
 
+		// @RequestParam 어노테이션
+		// HttpServletRequest 객체생성 및 getParameter 메소드 대체
+		// required = ture 속성 : 필수값 지정 (값 없다면 Exception) 
+		// defaultValue = "" 속성 : required = false 일 때, 값 없는 경우 기본값 지정 
+		
 		memberService.select(email, pwd);
 
-		model.addAttribute("email", email);
-		model.addAttribute("pwd", pwd);
-
-		return "siginOk";
+		try {
+			model.addAttribute("email", email);
+			model.addAttribute("pwd", pwd);			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "signinOk";
 	}
 
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(Model model, HttpServletRequest request) {
-		String email = request.getParameter("email");
-		String pwd = request.getParameter("pwd");
-		String username = request.getParameter("username");
-		String cell1 = request.getParameter("cell1");
-		String cell2 = request.getParameter("cell2");
-		String cell3 = request.getParameter("cell3");
+	public String signup(Member member) {
+		// 커맨드 객체를 이용한 HTTP 전송 정보 얻기 
+		// Member (DTO) 객체 생성시, setter 자동실행 (POST 값이 이름이 동일한 변수에 저장됨)
+		
+//		String email = request.getParameter("email");
+//		String pwd = request.getParameter("pwd");
+//		String username = request.getParameter("username");
+//		String cell1 = request.getParameter("cell1");
+//		String cell2 = request.getParameter("cell2");
+//		String cell3 = request.getParameter("cell3");
 
-		memberService.register(email, pwd, username, cell1, cell2, cell3);
-
-		model.addAttribute("email", email);
-		model.addAttribute("pwd", pwd);
-		model.addAttribute("username", username);
-		model.addAttribute("cell", cell1 + "-" + cell2 + "-" + cell3);
+		memberService.register(member.getEmail(), member.getPwd(), member.getUsername(), member.getCell1(), member.getCell2(), member.getCell3());
+//
+//		model.addAttribute("email", email);
+//		model.addAttribute("pwd", pwd);
+//		model.addAttribute("username", username);
+//		model.addAttribute("cell", cell1 + "-" + cell2 + "-" + cell3);
 
 		return "signupOk";
 	}
