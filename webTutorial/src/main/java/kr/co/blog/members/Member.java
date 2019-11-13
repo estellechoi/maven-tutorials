ef8d5b21703fd03320cad2da48c415dd7f5faead
youjin <youjin@192.168.31.152>
2019-11-13 11:17:17 +0900

    데이터베이스 회원 테이블 각 필드에 추가할 값들을 변수로 가진 DTO 클래

---
+++ webTutorial/src/main/java/kr/co/blog/members/Member.java
@@ -0,0 +1,37 @@
+package kr.co.blog.members;
+
+public class Member {
+	
+	private String email;
+	private String pwd;
+	private String username;
+	private String cell;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getCell() {
+		return cell;
+	}
+	public void setCell(String cell) {
+		this.cell = cell;
+	}
+	
+	
+	
+
+}
