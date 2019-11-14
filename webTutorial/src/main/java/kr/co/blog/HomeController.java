c14b4df2837317f69f38891ac8ba6bea07eafa77
youjin <youjin@choeyujcbookpro>
2019-11-14 20:52:49 +0900

    CookieValue@ 쿠키 가져오기 / new Cookie("cookieName", "value"); 쿠키 객체 생성

--- webTutorial/src/main/java/kr/co/blog/HomeController.java
+++ webTutorial/src/main/java/kr/co/blog/HomeController.java
@@ -1,39 +1,60 @@
 package kr.co.blog;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.co.blog.shop.Shop;
+
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+	@RequestMapping("/")
+	public String home(Shop shop,
+			@CookieValue(value = "gender", required = false) Cookie genderCookie,
+			HttpServletRequest request) {
+		// @CookieValue(value="name") Cookie cookieName
+		// 동일 이름의 쿠키를 가져와서 쿠키객체 cookieName 에 저장 (동일이름 쿠키 없다면 Exception) 
+		// Exception 피하기 -> required = false (디폴트값 true)
+		if (genderCookie != null) {
+			shop.setGender(genderCookie.getValue());
+		}
 		
 		return "home"; // .jsp 생략 
 	}
+	
+	@RequestMapping(value = "/homeOk", method = RequestMethod.POST)
+	public String homeOk(Shop shop, HttpServletResponse response) {
+		
+		// 쿠키 객체 생성
+		// 이름이 gender 이고 값은 shop.getGender()인 쿠키를 genderCookie 객체에 저장 
+		Cookie genderCookie = new Cookie("gender", shop.getGender());
+		
+		if(shop.getIsCookieDel()) {
+			// 쿠키를 생성하지 않는다 (유효시간 : 0)
+			genderCookie.setMaxAge(0);
+			shop.setGender(null);
+		}
+		else {
+			// 쿠키 생성 (유효시간 지정)
+			genderCookie.setMaxAge(60*60*24*30);
+		}	
+		// (클라이언트에게) response에 쿠키 추가 
+		// 클라이언트의 로컬 PC에 쿠키가 저장됨 
+		response.addCookie(genderCookie);
+		
+		return "homeOk"; // .jsp 생략 
+	}
+	
+	
 	
 }
