79e1c1955b3ab686cda213a100f2eb2c98d488b2
youjin <youjin@choeyujcbookpro>
2019-11-14 20:54:58 +0900

    private boolean isCookieDel;

---
+++ webTutorial/src/main/java/kr/co/blog/shop/Shop.java
@@ -0,0 +1,24 @@
+package kr.co.blog.shop;
+
+public class Shop {
+
+	private String gender;
+	private boolean isCookieDel;
+	
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public boolean getIsCookieDel() {
+		return isCookieDel;
+	}
+	public void setIsCookieDel(boolean isCookieDel) {
+		this.isCookieDel = isCookieDel;
+	}
+	
+	
+	
+	
+}
