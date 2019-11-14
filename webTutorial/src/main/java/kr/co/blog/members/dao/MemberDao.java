7681be0764f4dd0c05294b886cd583b4663f462b
youjin <youjin@192.168.31.152>
2019-11-14 16:49:07 +0900

    DAO / HashMap.put() HashMap.keySet() HashMap.get(key) Set.iterator()

--- webTutorial/src/main/java/kr/co/blog/members/dao/MemberDao.java
+++ webTutorial/src/main/java/kr/co/blog/members/dao/MemberDao.java
@@ -2,12 +2,14 @@ package kr.co.blog.members.dao;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import kr.co.blog.members.Member;
+import kr.co.blog.members.MemberCell;
 
 // 이 클래스를 스프링컨테이너에 빈 객체로 생성한다. 
 @Component
@@ -25,13 +27,13 @@ public class MemberDao implements iMemberDao {
 	}
 
 	@Override
-	public void insert(String email, String pwd, String username, String cell1, String cell2, String cell3) {
-		// DTO 객체의 각 변수에 값 주기 
+	public void insert(String email, String pwd, String username, List<MemberCell> cells, boolean isAdult, String[] sports) {
+		// DTO 객체의 각 변수에 값 주기
+		// -> 컨트롤러에서 커맨드 객체를 사용하여 대체 (매개변수로 DTO 객체 사용)
 		Member member = new Member();
-		member.setEmail(email);
-		member.setPwd(pwd);
-		member.setUsername(username);
-		member.setCell(cell1 + "-" + cell2 + "-" + cell3);
+//		member.setEmail(email);
+//		member.setPwd(pwd);
+//		member.setUsername(username);
 
 		// dbMap 에 데이터 추가 (key, value)
 		dbMap.put(email, member);
@@ -54,15 +56,25 @@ public class MemberDao implements iMemberDao {
 	}
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
+	public Member[] update(Member member) {
+		
+		Member members[] = new Member[2];
+		// 이전 레코드 
+		members[0] = dbMap.get(member.getEmail());
+		
+		// 수정 후 레코드 
+		dbMap.put(member.getEmail(), member);
+		members[1] = dbMap.get(member.getEmail());
+		
+		return members;
 	}
 
 	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
-
+	public Map<String, Member> delete(Member member) {
+		// DB 에서 key 값으로 레코드 삭제 
+		dbMap.remove(member.getEmail());
+		// 레코드가 삭제된 DB 객체 리턴 
+		return dbMap;
 	}
 
 }
--- webTutorial/src/main/java/kr/co/blog/members/dao/iMemberDao.java
+++ webTutorial/src/main/java/kr/co/blog/members/dao/iMemberDao.java
@@ -1,12 +1,16 @@
 package kr.co.blog.members.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import kr.co.blog.members.Member;
+import kr.co.blog.members.MemberCell;
 
 public interface iMemberDao {
 	
-	void insert(String email, String pwd, String username, String cell1, String cell2, String cell3);
+	void insert(String email, String pwd, String username, List<MemberCell> cells, boolean isAdult, String[] sports);
 	Member select(String email, String pwd);
-	void update();
-	void delete();
+	Member[] update(Member member);
+	Map<String, Member> delete(Member member);
 
 }
