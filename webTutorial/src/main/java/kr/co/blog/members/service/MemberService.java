6d202c41d72a7c059b8e7cb3564a8f3123080413
youjin <youjin@192.168.31.152>
2019-11-14 16:50:25 +0900

    DAO 메소드를 호출하는 Service 클래스 / Service@ 스프링컨테이너에 빈 자동 생성

--- webTutorial/src/main/java/kr/co/blog/members/service/MemberService.java
+++ webTutorial/src/main/java/kr/co/blog/members/service/MemberService.java
@@ -1,11 +1,15 @@
 package kr.co.blog.members.service;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import kr.co.blog.members.Member;
+import kr.co.blog.members.MemberCell;
 import kr.co.blog.members.dao.MemberDao;
 
 // 이 클래스를 스프링컨테이너에 빈 객체로 생성한다. (스프링설정파일에서 bean 태그로 생성할 필요 X)
@@ -21,21 +25,13 @@ public class MemberService implements iMemberService {
 	MemberDao memberDao;
 
 	@Override
-	public void register(String email, String pwd, String username, String cell1, String cell2, String cell3) {
-		System.out.println("Member Register");
-		System.out.println("Email : " + email);
-		System.out.println("Pwd : "+ pwd);
-		System.out.println("Username : " + username);
-		System.out.println("Cell : " + cell1 + "-" + cell2 + "-" + cell3);
+	public void register(String email, String pwd, String username, List<MemberCell> cells, boolean isAdult, String[] sports) {
 		
-		memberDao.insert(email, pwd, username, cell1, cell2, cell3);
+		memberDao.insert(email, pwd, username, cells, isAdult, sports);
 	}
 
 	@Override
 	public Member select(String email, String pwd) {
-		System.out.println("Member Search");
-		System.out.println("Email : "+ email);
-		System.out.println("Pwd : "+ pwd);
 		
 		Member member = memberDao.select(email, pwd);
 		
@@ -43,15 +39,38 @@ public class MemberService implements iMemberService {
 	}
 
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
+	public Member[] edit(Member member) {
 		
+		Member members[] = memberDao.update(member);
+		
+		return members;
 	}
 
 	@Override
-	public void delete() {
-		// TODO Auto-generated method stub
+	public void remove(Member member) {
+		// memberDao 의 delete() 메소드 실행 -> 리턴값(dbMap)을 매개로 printMembers() 메소드 실행 
+		printMembers(memberDao.delete(member));	
+	}
+	
+	public void printMembers(Map<String, Member> map) {
 		
+		Set<String> keys = map.keySet();
+		Iterator<String> iterator = keys.iterator();
+		
+		while(iterator.hasNext()) {
+			String key = iterator.next();
+			Member member = map.get(key); // key 값으로 value(Member 타입) 가져오기 
+			printMember(member); // 탈퇴처리된 회원 제외하고 각 회원정보 출력 
+		}
+	}
+	
+	public void printMember(Member member) {
+		System.out.println("Email : " + member.getEmail());
+		System.out.println("Username : " + member.getUsername());
+		System.out.println("Pwd : " + member.getPwd());
+		System.out.println("Cell : " + member.getCells());
+		System.out.println("IsAdult : " + member.getIsAdult());
+		System.out.println("Sports : " + member.getSports());
 	}
 	
 	
--- webTutorial/src/main/java/kr/co/blog/members/service/iMemberService.java
+++ webTutorial/src/main/java/kr/co/blog/members/service/iMemberService.java
@@ -1,11 +1,14 @@
 package kr.co.blog.members.service;
 
+import java.util.List;
+
 import kr.co.blog.members.Member;
+import kr.co.blog.members.MemberCell;
 
 public interface iMemberService {
 
-	void register(String email, String pwd, String username, String cell1, String cell2, String cell3);
+	void register(String email, String pwd, String username, List<MemberCell> cells, boolean isAdult, String[] sports);
 	Member select(String email, String pwd); // 리턴 메소드 
-	void update();
-	void delete();
+	Member[] edit(Member member);
+	void remove(Member member);
 }
