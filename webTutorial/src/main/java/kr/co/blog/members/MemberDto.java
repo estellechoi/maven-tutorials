7817ebdd4112e50dceaad9466a727abdc56f12a1
youjin <youjin@192.168.31.152>
2019-11-18 13:36:03 +0900

    jdbcTemplate 학습을 위해 변수를 단순화한 DTO 커맨드 객체

---
+++ webTutorial/src/main/java/kr/co/blog/members/MemberDto.java
@@ -0,0 +1,29 @@
+package kr.co.blog.members;
+
+public class MemberDto {
+	
+	private String email;
+	private String pwd;
+	private String username;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	
+}
