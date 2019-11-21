f6ff40d0e45e0a4121124fca2211edbd223474b1
Estelle Choi <estele.choi@gmail.com>
2019-11-21 10:37:57 +0900

    의존주입된 bean 객체 호출 / 매개변수의 수가 동일한 생성자가 다수일 때 우선순위는 String > double > int

--- DependencyInjection/src/main/java/kr/co/spring/DependencyInjection/main/Main.java
+++ DependencyInjection/src/main/java/kr/co/spring/DependencyInjection/main/Main.java
@@ -2,7 +2,9 @@ package kr.co.spring.DependencyInjection.main;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import kr.co.spring.DependencyInjection.beans.DataBean;
 import kr.co.spring.DependencyInjection.beans.TestBean;
+import kr.co.spring.DependencyInjection.beans.TestBean2;
 
 public class Main {
 
@@ -22,6 +24,23 @@ public class Main {
 		TestBean testBeanJava4 = new TestBean("hello world!");
 		testBeanJava4.printData();
 
+		TestBean testBeanJava5 = new TestBean(100, 11.1, "hello world!");
+		testBeanJava5.printData();
+
+		// 자바코드로 객체 생성시, 생성자 매개변수의 자료형과 순서가 다르면 오류 
+		/*
+		TestBean testBeanJava6 = new TestBean("hello world!", 100, 11.1);
+		testBeanJava6.printData();
+		*/
+		
+		// 생성자 매개변수에 들어갈 값이 클래스 객체일 때 
+		DataBean d1 = new DataBean();
+		DataBean d2 = new DataBean();
+		TestBean2 testBeanJava8 = new TestBean2(d1, d2);
+		testBeanJava8.print();
+		
+		
+		
 		
 		// 2) 스프링 설정파일 사용하여 객체 생성 및 메소드 호출 
 		
@@ -44,6 +63,28 @@ public class Main {
 		TestBean testBeanSpring4 = ctx.getBean("testBean4", TestBean.class);
 		testBeanSpring4.printData();
 
+		// 매개변수 3개 일 때 
+		TestBean testBeanSpring5 = ctx.getBean("testBean5", TestBean.class);
+		testBeanSpring5.printData();
+
+		// 생성자 매개변수 자료형 순서와 xml 에서 bean 정의시 constructor-arg 자료형 순서가 다를 때 
+		// 작성순서와 무관하게 자료형 맞추어 자동 주입 
+		TestBean testBeanSpring6 = ctx.getBean("testBean6", TestBean.class);
+		testBeanSpring6.printData();
+
+		// 자료형이 같은 생성자 매개변수가 복수일 때, index 로 순서 지정 
+		// index 미지정시, 작성한 순서대로 주입 
+		TestBean testBeanSpring7 = ctx.getBean("testBean7", TestBean.class);
+		testBeanSpring7.printData();
+		
+		// 생성자 매개변수에 들어갈 값이 클래스 객체일 때 (의존주입시 bean 정의)
+		TestBean2 testBeanSpring8 = ctx.getBean("testBean8", TestBean2.class);
+		testBeanSpring8.print();
+		
+		// 생성자 매개변수에 들어갈 값이 클래스 객체일 때 (이미 정의된 bean 의존주입)
+		TestBean2 testBeanSpring9 = ctx.getBean("testBean9", TestBean2.class);
+		testBeanSpring9.print();
+		
 	}
 
 }
