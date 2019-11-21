c4e46acca44a876f46bfe99aaa24c90d5e411c13
Estelle Choi <estele.choi@gmail.com>
2019-11-21 21:19:53 +0900

    new AnnotationConfigApplicationContext(BeanConfiguration.class)

--- SpringDI/src/main/java/kr/co/spring/SpringDI/main/Main.java
+++ SpringDI/src/main/java/kr/co/spring/SpringDI/main/Main.java
@@ -3,6 +3,8 @@ package kr.co.spring.SpringDI.main;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import kr.co.spring.SpringDI.beans.Member;
+import kr.co.spring.SpringDI.beans.TestBean;
+import kr.co.spring.SpringDI.beans.TestBean2;
 import kr.co.spring.SpringDI.config.BeanConfiguration;
 
 public class Main {
@@ -27,7 +29,25 @@ public class Main {
 		System.out.printf("member2.age : %d\n", member2.getAge());
 		System.out.printf("member2.email : %s\n", member2.getEmail());
 		System.out.printf("member2.group : %s\n", member2.getGroup());
+		
+		System.out.println("-------------------------------------------------------");
 
+		// 3) 자동주입 설정된 bean 호출 (Autowire.BY_NAME)
+		TestBean testBean = ctx.getBean("testBean", TestBean.class);
+		
+		System.out.printf("testBean.data1 : %s\n", testBean.getData1());
+		System.out.printf("testBean.data2 : %s\n", testBean.getData2());
+
+		System.out.println("-------------------------------------------------------");		
+		
+		// 4) 자동주입 설정된 bean 호출 (Autowire.BY_TYPE)
+		TestBean2 testBean2 = ctx.getBean("testBean2", TestBean2.class);
+		
+		System.out.printf("testBean2.data1 : %s\n", testBean2.getData1());
+		System.out.printf("testBean2.data2 : %s\n", testBean2.getData2());
+
+		
+		
 		ctx.close();
 	}
 
