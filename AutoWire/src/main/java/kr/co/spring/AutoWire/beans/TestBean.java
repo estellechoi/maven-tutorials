3536bc8074eff71336ad34b132b67c92db249f19
Estelle Choi <estele.choi@gmail.com>
2019-11-21 16:18:30 +0900

    Member 클래스와 Group 클래스를 타입으로 하는 변수들

---
+++ AutoWire/src/main/java/kr/co/spring/AutoWire/beans/TestBean.java
@@ -0,0 +1,38 @@
+package kr.co.spring.AutoWire.beans;
+
+public class TestBean {
+	
+	private Member member1;
+	private Member member2;
+	private Group group1;
+	private Group group2;
+	
+	public Group getGroup1() {
+		return group1;
+	}
+	public void setGroup1(Group group1) {
+		this.group1 = group1;
+	}
+	public Group getGroup2() {
+		return group2;
+	}
+	public void setGroup2(Group group2) {
+		this.group2 = group2;
+	}
+	public Member getMember1() {
+		return member1;
+	}
+	public void setMember1(Member member1) {
+		this.member1 = member1;
+	}
+	public Member getMember2() {
+		return member2;
+	}
+	public void setMember2(Member member2) {
+		this.member2 = member2;
+	}
+	
+	
+	
+
+}
