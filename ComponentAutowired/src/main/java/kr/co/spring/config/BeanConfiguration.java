e31787fd8c589e98babbdedb1c4975980daa9f3b
Estelle Choi <estele.choi@gmail.com>
2019-11-22 15:39:55 +0900

    @ComponentScan(basePackages = "패키지 경로") , @ Bean 등록시 필수 프로퍼티 주입

---
+++ ComponentAutowired/src/main/java/kr/co/spring/config/BeanConfiguration.java
@@ -0,0 +1,27 @@
+package kr.co.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import kr.co.spring.component.DataBean;
+import kr.co.spring.component.FlowBean;
+import kr.co.spring.component.NameBean;
+import kr.co.spring.component.ServiceBean3;
+
+@Configuration
+@ComponentScan(basePackages = "kr.co.spring.component")
+public class BeanConfiguration {
+
+	@Bean
+	public NameBean name2() {
+		return new NameBean();
+	}
+	
+	// ServiceBean3 의 bean 을 여러개 등록하자.
+	@Bean
+	public ServiceBean3 serviceBean3() {
+		// ServiceBean3 클래스에서 생성자의 매개변수를 통해 필수로 주입할 프로퍼티를 설정하였다. 
+		return new ServiceBean3(24, "youyoung", new DataBean(), new FlowBean());
+	}
+}
