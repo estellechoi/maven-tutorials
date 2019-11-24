7bee352ca04b9938fe4f9c65caa13c1a6c8e1521
Estelle Choi <estele.choi@gmail.com>
2019-11-24 17:25:56 +0900

    @EnableAspectJAutoProxy : @ aspect 클래스에 설정된 어노테이션을 분석하여 AOP 설정한다.

---
+++ AOPAnnotation/src/main/java/kr/co/spring/config/BeansConfiguration.java
@@ -0,0 +1,13 @@
+package kr.co.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = { "kr.co.spring.component", "kr.co.spring.advisor" })
+// @EnableAspectJAutoProxy : Advisor 클래스에 설정된 어노테이션을 분석하여 AOP 를 세팅한다.
+@EnableAspectJAutoProxy
+public class BeansConfiguration {
+
+}
